12dea67c300e96299370affa88d997f4fe1696aa
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-05-08 10:00:51 -0700

    Cody demos

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { message } from './index.js'
+
+const originalMessage: string = message
+
+const newMessage: string = 'Hello from typescript'
+
+export {
+    originalMessage
+}
+
+export default newMessage
